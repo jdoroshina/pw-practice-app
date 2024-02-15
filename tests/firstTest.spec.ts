f48e2e2d3d6225381a4f731bfd2e9ec27e00c138
julia doroshina <yuspiri@gmail.com>
2024-02-15 13:14:12 +0100

    feat: add test navigation to datepicker

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -6,3 +6,9 @@ test('the first test', async ({page}) => {
   await page.getByText('Form Layouts').click()
 })
 
+test('navigate to datepicker page', async ({page}) => {
+  await page.goto('http://localhost:4200/')
+  await page.getByText('Forms').click()
+  await page.getByText('Datepicker').click()
+})
+
