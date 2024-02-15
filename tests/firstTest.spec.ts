2dfe9ee2b36804eabb1513dbe5d4a20dcc5afc14
julia doroshina <yuspiri@gmail.com>
2024-02-15 13:55:05 +0100

    feat: add second test and group tests

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -2,14 +2,28 @@ import {test} from '@playwright/test'
 
 test.beforeEach( async ({page}) => {
   await page.goto('http://localhost:4200/')
-  await page.getByText('Forms').click()
 })
 
-test('the first test', async ({page}) => {
-  await page.getByText('Form Layouts').click()
+test.describe('suite', () => {
+  test.beforeEach( async ({page}) => {
+    await page.getByText('Forms').click()
+  })
+
+  test('the first test', async ({page}) => {
+    await page.getByText('Form Layouts').click()
+  })
+
+  test('navigate to datepicker page', async ({page}) => {
+    await page.getByText('Datepicker').click()
+  })
 })
 
-test('navigate to datepicker page', async ({page}) => {
-  await page.getByText('Datepicker').click()
-})
+test.describe('suite2', () => {
+  test.beforeEach( async ({page}) => {
+    await page.getByText('Charts').click()
+  })
 
+  test('navigate to echarts page', async ({page}) => {
+    await page.getByText('Echarts').click()
+  })
+})
