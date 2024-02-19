b0c4adc2e7c22b9b717d14edaded1642f8adc502
julia doroshina <yuspiri@gmail.com>
2024-02-19 10:54:00 +0100

    feat: add all types of element locators

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -2,28 +2,35 @@ import {test} from '@playwright/test'
 
 test.beforeEach( async ({page}) => {
   await page.goto('http://localhost:4200/')
+  await page.getByText('Forms').click()
+  await page.getByText('Form Layouts').click()
 })
 
-test.describe('suite', () => {
-  test.beforeEach( async ({page}) => {
-    await page.getByText('Forms').click()
-  })
+test('Locator syntax rules', async({page}) => {
+  //by Tag name
+  page.locator('input')
 
-  test('the first test', async ({page}) => {
-    await page.getByText('Form Layouts').click()
-  })
+  //by ID
+  await page.locator('#inputEmail1').click()
 
-  test('navigate to datepicker page', async ({page}) => {
-    await page.getByText('Datepicker').click()
-  })
-})
+  //by Class value
+  page.locator('.shape-rectangle')
 
-test.describe('suite2', () => {
-  test.beforeEach( async ({page}) => {
-    await page.getByText('Charts').click()
-  })
+  //by attribute
+  page.locator('[placeholder="Email"]')
 
-  test('navigate to echarts page', async ({page}) => {
-    await page.getByText('Echarts').click()
-  })
+  //by entire Class value (full)
+  page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+  //combine different selectors
+  page.locator('input[placeholder="Email"].shape-rectangle')
+
+  //by Xpath (Not recommended)
+  page.locator('//*[@id="inputEmail1"]')
+
+  //partial text match
+  page.locator(':text("Using")')
+
+  //by exact text match
+  page.locator(':text-is("Using the Grid")')
 })
