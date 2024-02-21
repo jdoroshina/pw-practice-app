7481106c4d3fa80ee6ffa47dc6749e6b8b7e4773
julia doroshina <yuspiri@gmail.com>
2024-02-21 15:09:34 +0100

    feat: test radio buttons

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -1,4 +1,5 @@
 import {test, expect} from '@playwright/test'
+import { using } from 'rxjs'
 
 test.beforeEach(async({page}) => {
   await page.goto('http://localhost:4200/')
@@ -23,5 +24,19 @@ test.describe('Form Layouts page', () => {
 
     //locator assertion
     await expect(usingTheGridEmailInput).toHaveValue('test2@test.com')
+  })
+
+  test('radio buttons', async ({page}) => {
+    const usingTheGridForm = page.locator('nb-card', {hasText: "USing the Grid"})
+
+    // await usingTheGridEmailInput.getByLabel('Option 1').check({force: true}) //in order to bypass this validation of the availability (element is visually-hidden), we need to pass the parameter force.
+    await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).check({force: true}) //to select the radio button, the most recommended way is to use Get By role radio and provide the name of the radio button. & To select a radio button use method .check()
+    const radioStatus = await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked()
+    expect(radioStatus).toBeTruthy()
+    await expect(usingTheGridForm.getByRole('radio', {name: 'Option 1'})).toBeChecked()
+
+    await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).check({force: true})
+    expect(await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked()).toBeFalsy()
+    expect(await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).isChecked()).toBeTruthy()
   })
 })
