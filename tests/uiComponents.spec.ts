963970f233d01a5b2bb87bf9dedf8e43310be100
julia doroshina <yuspiri@gmail.com>
2024-02-28 12:34:53 +0100

    feat: test checkboxes

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -12,7 +12,7 @@ test.describe('Form Layouts page', () => {
   })
 
   test('input fields', async({page}) => {
-    const usingTheGridEmailInput = page.locator('nb-card', {hasText: "USing the Grid"}).getByRole('textbox', {name: "Email"})
+    const usingTheGridEmailInput = page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"})
 
     await usingTheGridEmailInput.fill('test@test.com')
     await usingTheGridEmailInput.clear()
@@ -27,7 +27,7 @@ test.describe('Form Layouts page', () => {
   })
 
   test('radio buttons', async ({page}) => {
-    const usingTheGridForm = page.locator('nb-card', {hasText: "USing the Grid"})
+    const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
 
     // await usingTheGridEmailInput.getByLabel('Option 1').check({force: true}) //in order to bypass this validation of the availability (element is visually-hidden), we need to pass the parameter force.
     await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).check({force: true}) //to select the radio button, the most recommended way is to use Get By role radio and provide the name of the radio button. & To select a radio button use method .check()
@@ -39,4 +39,18 @@ test.describe('Form Layouts page', () => {
     expect(await usingTheGridForm.getByRole('radio', {name: 'Option 1'}).isChecked()).toBeFalsy()
     expect(await usingTheGridForm.getByRole('radio', {name: 'Option 2'}).isChecked()).toBeTruthy()
   })
+})
+
+test('checkboxes', async({page}) => {
+  await page.getByText('Modal & Overlays').click()
+  await page.getByText('Toastr').click()
+
+  await page.getByRole('checkbox', {name: "Hide on click"}).uncheck({force: true})
+  await page.getByRole('checkbox', {name: "Prevent arising of duplicate toast"}).check({force: true})
+
+  const allBoxes = page.getByRole('checkbox')
+  for(const box of await allBoxes.all()) {// call a method .all() - will create an array out of the list of this element
+    await box.check({force: true})
+    expect(await box.isChecked()).toBeTruthy()
+  }
 })
